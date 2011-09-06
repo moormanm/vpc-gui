c4aa6ab948fc278c79d694f9b65cf9bc2be643b2
michael.e.moorman@gmail.com <michael.e.moorman@gmail.com@819135f5-631c-e3a1-067e-df997026853d>
2011-09-06 19:44:01 +0000

    initial effort

---
+++ src/Defaults.java
@@ -0,0 +1,10 @@
+import javax.swing.plaf.FontUIResource;
+import java.awt.Font;
+
+
+public class Defaults {
+  public static final FontUIResource FONT = new FontUIResource(new Font("tahoma", Font.PLAIN, 12 ));
+  public static final FontUIResource TITLE_FONT = new FontUIResource(new Font("tahoma", Font.BOLD, 24 ));
+  
+}
+
